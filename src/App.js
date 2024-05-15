cbff8ad22f863b4419042609103d8f5e0f4cf8d2
Camila Ramos <camilaafonsolemes@gmail.com>
2024-05-15 16:39:11 -0300

    Changes: - inserting external data with fetch

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import './App.css';
 
 class App extends Component {
   state = {
-    posts: [
-      {
-        id: 1,
-        title: 'O titulo',
-        body: 'O corpo'
-      },
-    ]
+    posts: []
   };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(posts => this.setState({posts}))
+  }
 
   render() {
     const { posts } = this.state;
@@ -20,8 +20,8 @@ class App extends Component {
       <div className="App">
         {posts.map(post => (
           <div key={post.id}>
-          <h1>{post.title}</h1>
-          <p>{post.body}</p>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
           </div>
         ))}
       </div>
