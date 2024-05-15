3100e07e50840d05d0172ec529eda747e6f536c6
Camila Ramos <camilaafonsolemes@gmail.com>
2024-05-15 16:23:21 -0300

    Changes to App.js: - inclusion of import {component} from react; - render embedded {posts};

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: 'O titulo',
+        body: 'O corpo'
+      },
+    ]
+  };
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div className="App">
+        {posts.map(post => (
+          <div key={post.id}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 export default App;
